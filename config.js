e2e8cdc8936f0f6d20bb832740cec66defd4cdc4
ThunderPc2 <e04561029@gmail.com>
2024-06-12 13:39:08 +0700

    Add files via upload

---
+++ config.js
@@ -0,0 +1,24 @@
+const CONFIG = {
+  PRODUCTS: [
+    {
+      name: 'Ролл Классический',
+      cost: 270,
+      preview: '1.png'
+    },
+    {
+      name: 'Ролл Аппетитный',
+      cost: 400,
+      preview: '2.jpg'
+    },
+    {
+      name: 'Ролл Сашими',
+      cost: 280,
+      preview: '3.jpg'
+    },
+    {
+      name: 'Ролл Премиальный',
+      cost: 800,
+      preview: '4.jpg'
+    }
+  ]
+}
